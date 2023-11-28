25978b223df29a2492f4ff3eb48f9f8a4fa90601
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-11-29 00:36:22 +0600

    middlewares created

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000;
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
 
 
-// middlewares
+
+// middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -27,24 +28,63 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const serviceCollection = client.db("eHubDb").collection("services");
         const queryCollection = client.db("eHubDb").collection("queries");
         const userCollection = client.db("eHubDb").collection("users");
 
 
-         // jwt related api
-         app.post('/jwt', async (req, res) => {
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.send({ token });
         })
 
+        // middlewares
+        const verifyToken = (req, res, next) => {
+            // console.log("Token to Verify", req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            console.log(token);
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'Unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
 
-        
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === "admin";
+            if (!isAdmin) {
+                return res.status(403).send({ message: "Forbidden" })
+            }
+            next();
+        }
 
 
+        const verifyHr = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isHr = user?.role === "hr";
+            if (!isHr) {
+                return res.status(403).send({ message: "Forbidden" })
+            }
+            next();
+        }
+
+
+
+        // service related api
 
         app.get('/services', async (req, res) => {
             const result = await serviceCollection.find().toArray();
@@ -59,6 +99,11 @@ async function run() {
 
         // user related api
 
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const item = req.body;
             const result = await userCollection.insertOne(item);
@@ -68,7 +113,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
